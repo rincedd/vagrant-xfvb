fc71e616d93185d3e1e24da24c64a42883e1542a
Gerd Zschaler <gerd@gzschaler.de>
2015-01-21 20:53:48 +0100

    Use willdurand-nodejs for now, as puppetlabs-nodejs is currently broken

--- puppet/site.pp
+++ puppet/site.pp
@@ -2,17 +2,22 @@ package { 'git':
   ensure => installed,
 }
 
-#class { 'nodejs':; }
-#
-#package { 'grunt-cli':
-#  provider => 'npm',
-#  ensure => present
-#}
-#
-#package { 'bower':
-#  provider => 'npm',
-#  ensure => present
-#}
+class { 'nodejs':
+  version => 'stable',
+  make_install => false
+}
+
+package { 'grunt-cli':
+  provider => 'npm',
+  ensure => present,
+  require => Class['nodejs']
+}
+
+package { 'bower':
+  provider => 'npm',
+  ensure => present,
+  require => Class['nodejs']
+}
 
 class { 'java':
   distribution => 'jre'
